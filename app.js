80a587f8eda0de52bf21219eeca777c382928d35
Anna Richard <184364678+annahcodes@users.noreply.github.com>
2025-06-08 22:45:14 +0300

    Add logic for the simple color generator
    Generate random colors using the
    Math.random object.

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [
+	'green',
+	'red',
+	'rgba(133,122,200)',
+	'#f15025',
+	`DarkSalmon`,
+	`DarkSeaGreen`,
+	`DarkSlateBlue`,
+	`DarkSlateGray`,
+	`DarkSlateGrey`,
+	`DarkTurquoise`,
+	`DarkViolet`,
+	`DeepPink`,
+	`DeepSkyBlue`,
+	`DimGray`,
+	`DimGrey`,
+	`DodgerBlue`,
+	`FireBrick`,
+	`FloralWhite`,
+	`ForestGreen`,
+	`Fuchsia`,
+	`Gainsboro`,
+	`GhostWhite`,
+	`Gold`,
+	`GoldenRod`,
+	`Gray`,
+
+];
+
+// ======select elements
+const button = document.querySelector('#btn');
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+
+// ===select random index
+function getRandomIndex() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return randomIndex;
+}
+
+// ===add event listener
+button.addEventListener('click', function () {
+  const randomColor = getRandomIndex();
+  body.style.backgroundColor = colors[randomColor];
+  span.textContent = colors[randomColor];
+
+});
